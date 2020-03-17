51833eaa363c3b772b37dcd0494dde44c037d5db
Kykung <59070071@it.kmitl.ac.th>
2020-03-18 02:16:03 +0700

    Remove some metrics

--- config.py
+++ config.py
@@ -7,12 +7,6 @@ ITERATION = 5
 BATCH_SIZE = 16
 EPOCHS = 500
 METRICS = [
-      keras.metrics.TruePositives(name='tp'),
-      keras.metrics.FalsePositives(name='fp'),
-      keras.metrics.TrueNegatives(name='tn'),
-      keras.metrics.FalseNegatives(name='fn'), 
-      keras.metrics.Precision(name='precision'),
-      keras.metrics.Recall(name='recall'),
       keras.metrics.AUC(name='auc'),
 ]
 EARLY_STOPPING = keras.callbacks.EarlyStopping(
