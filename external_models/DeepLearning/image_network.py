42db040f8872cc5983b06bf3732f1bc25d7ce896
Kykung <59070071@it.kmitl.ac.th>
2020-03-21 15:30:47 +0700

    Adjust image network

--- external_models/DeepLearning/image_network.py
+++ external_models/DeepLearning/image_network.py
@@ -3,17 +3,14 @@ from tensorflow import keras
 
 from config import LOSS, METRICS, OPTIMIZER
 
-def make_model(input_shape, loss, metrics = METRICS, optimizer = OPTIMIZER, output_bias=None):
-
-    if output_bias is not None:
-        output_bias = tf.keras.initializers.Constant(output_bias)
+def make_model(input_shape, loss, metrics = METRICS, optimizer = OPTIMIZER):
 
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=input_shape),
         keras.layers.Dense(1000, activation='relu'),
         keras.layers.Dense(100, activation='relu'),
         keras.layers.Dense(10, activation='relu'),
-        keras.layers.Dense(1, activation='sigmoid', bias_initializer=output_bias),
+        keras.layers.Dense(2),
     ])
 
     model.compile(
