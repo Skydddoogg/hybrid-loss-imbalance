a56d700beb0f9cd0163b480a684ae0663a2c1c99
Kykung <59070071@it.kmitl.ac.th>
2020-03-25 02:21:51 +0700

    Adjust the image network

--- external_models/DeepLearning/image_network.py
+++ external_models/DeepLearning/image_network.py
@@ -11,8 +11,11 @@ def make_model(input_shape, loss, metrics = METRICS, optimizer = OPTIMIZER, outp
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=input_shape),
         keras.layers.Dense(1000, activation='relu'),
+        keras.layers.BatchNormalization(),
         keras.layers.Dense(100, activation='relu'),
+        keras.layers.BatchNormalization(),
         keras.layers.Dense(10, activation='relu'),
+        keras.layers.BatchNormalization(),
         keras.layers.Dense(1, activation='sigmoid', bias_initializer=output_bias),
     ])
 
