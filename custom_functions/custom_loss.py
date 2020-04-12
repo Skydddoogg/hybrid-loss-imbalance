f3fe87ed789083c56353feecdf764db268c8b203
Kykung <59070071@it.kmitl.ac.th>
2020-04-12 15:13:52 +0700

    Fix unusual loss error

--- custom_functions/custom_loss.py
+++ custom_functions/custom_loss.py
@@ -248,7 +248,7 @@ class Hybrid(object):
         negative_y_pred = math_ops.cast(tf.where(tf.less_equal(negative_y_pred, 0.5), 0.0, 1.0), tf.float32)
 
         false_neg = math_ops.cast(math_ops.reduce_sum(positive_y_true - positive_y_pred), tf.float32)
-        false_pos = math_ops.cast(math_ops.reduce_sum(negative_y_true - negative_y_pred), tf.float32)
+        false_pos = math_ops.cast(math_ops.reduce_sum(negative_y_pred - negative_y_true), tf.float32)
 
         positive_cost = tf.where(tf.equal(n_positive, 0), 0.0, math_ops.cast(false_neg / n_positive, tf.float32))
         negative_cost = tf.where(tf.equal(n_negative, 0), 0.0, math_ops.cast(false_pos / n_negative, tf.float32))
@@ -303,7 +303,7 @@ class Hybrid(object):
         negative_y_pred = math_ops.cast(tf.where(tf.less_equal(negative_y_pred, 0.5), 0.0, 1.0), tf.float32)
 
         false_neg = math_ops.cast(math_ops.reduce_sum(positive_y_true - positive_y_pred), tf.float32)
-        false_pos = math_ops.cast(math_ops.reduce_sum(negative_y_true - negative_y_pred), tf.float32)
+        false_pos = math_ops.cast(math_ops.reduce_sum(negative_y_pred - negative_y_true), tf.float32)
 
         positive_cost = tf.where(tf.equal(n_positive, 0), 0.0, math_ops.cast(false_neg / n_positive, tf.float32))
         negative_cost = tf.where(tf.equal(n_negative, 0), 0.0, math_ops.cast(false_pos / n_negative, tf.float32))
