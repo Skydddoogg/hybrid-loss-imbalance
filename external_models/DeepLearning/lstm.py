044029b59cede5410b44731f70518bda801f5a04
Kykung <59070071@it.kmitl.ac.th>
2020-05-28 00:21:48 +0700

    Adjust network

--- external_models/DeepLearning/lstm.py
+++ external_models/DeepLearning/lstm.py
@@ -3,37 +3,28 @@ from tensorflow.keras import layers
 
 def make_model(max_features):
 
-    embedding_dims = 50
-    filters = 250
-    kernel_size = 3
-    hidden_dims = 250
+    # Embedding
     maxlen = 200
+    embedding_size = 128
+
+    # Convolution
+    kernel_size = 5
+    filters = 64
+    pool_size = 4
+
+    # LSTM
+    lstm_output_size = 70
 
     model = keras.models.Sequential()
-
-    # we start off with an efficient embedding layer which maps
-    # our vocab indices into embedding_dims dimensions
-    model.add(layers.Embedding(max_features,
-                        embedding_dims,
-                        input_length=maxlen))
-    model.add(layers.Dropout(0.2))
-
-    # we add a Convolution1D, which will learn filters
-    # word group filters of size filter_length:
+    model.add(layers.Embedding(max_features, embedding_size, input_length=maxlen))
+    model.add(layers.Dropout(0.25))
     model.add(layers.Conv1D(filters,
                     kernel_size,
                     padding='valid',
                     activation='relu',
                     strides=1))
-    # we use max pooling:
-    model.add(layers.GlobalMaxPooling1D())
-
-    # We add a vanilla hidden layer:
-    model.add(layers.Dense(hidden_dims))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Activation('relu'))
-
-    # We project onto a single unit output layer, and squash it with a sigmoid:
+    model.add(layers.MaxPooling1D(pool_size=pool_size))
+    model.add(layers.LSTM(lstm_output_size))
     model.add(layers.Dense(1))
     model.add(layers.Activation('sigmoid'))
 
