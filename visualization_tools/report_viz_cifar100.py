a846c65b41720366be1ac47d710f274524d7d63d
Skydddoogg <59070071@it.kmitl.ac.th>
2020-06-08 21:10:47 +0700

    Edit viz tool

--- visualization_tools/report_viz_cifar100.py
+++ visualization_tools/report_viz_cifar100.py
@@ -3,7 +3,7 @@ sys.path.append("../")
 
 import numpy as np
 import os
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, auc, roc_curve
 import argparse
 from joblib import dump, load
 import pandas as pd
@@ -18,6 +18,7 @@ from config_path import result_path, viz_path
 def create_report(dataset_name, reduction_ratio, network, loss_list):
 
     f1_loss = {}
+    auc_loss = {}
     best_loss = None
     comparison_param = {
         'f1': -np.Infinity,
@@ -28,19 +29,28 @@ def create_report(dataset_name, reduction_ratio, network, loss_list):
 
         classification_algorithm = 'dl-' + loss
         f1_collector = []
+        auc_collector = []
 
         for _round in range(1, N_ROUND + 1):
             y_test = np.load(os.path.join(result_path, dataset_name, 'groundtruth', network + '_' + 'round_' + str(_round) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(reduction_ratio) + ".npy"))
             y_pred = np.load(os.path.join(result_path, dataset_name, 'prediction', network + '_' + 'round_' + str(_round) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(reduction_ratio) + ".npy"))
-
+            y_prob = np.load(os.path.join(result_path, dataset_name, 'probability', network + '_' + 'round_' + str(_round) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(reduction_ratio) + ".npy"))
+    
             cm = confusion_matrix(y_test, y_pred)
+            fpr, tpr, _ = roc_curve(y_test, y_prob)
+
 
             f1 = f1_score(y_test, y_pred)
+            _auc = auc(fpr, tpr)
 
             f1_collector.append(f1)
+            auc_collector.append(_auc)
 
         f1_collector = np.array(f1_collector)
         f1_loss[loss] = f1_collector
+
+        auc_collector = np.array(auc_collector)
+        auc_loss[loss] = auc_collector
 
         if np.average(f1_collector) > comparison_param['f1']:
             comparison_param['f1'] = np.average(f1_collector)
@@ -56,9 +66,11 @@ def create_report(dataset_name, reduction_ratio, network, loss_list):
 
     for loss in LOSS_LIST:
         if loss == best_loss:
-            print('%20s: %.2f±%.2f - Best' % (loss, np.average(f1_loss[loss]), np.std(f1_loss[loss])))
+            print('%20s: %.2f±%.2f (AUC = %.2f) - Best' % (loss, np.average(f1_loss[loss]) * 100, np.std(f1_loss[loss]), np.average(auc_loss[loss]) * 100))
         else:
-            print('%20s: %.2f±%.2f' % (loss, np.average(f1_loss[loss]), np.std(f1_loss[loss])))
+            print('%20s: %.2f±%.2f (AUC = %.2f)' % (loss, np.average(f1_loss[loss]) * 100, np.std(f1_loss[loss]), np.average(auc_loss[loss]) * 100))
+
+    return f1_loss, auc_loss
 
 
 if __name__ == '__main__':
@@ -81,16 +93,13 @@ if __name__ == '__main__':
         }
 
     LOSS_LIST = [
-        'Balanced-BCE',
         'MFE',
         'MSFE',
-        'FL',
         'Balanced-FL',
-        'Hybrid',
         'Balanced-Hybrid'
     ]
 
-    N_ROUND = 5
+    N_ROUND = 1
 
     REDUCTION_RATIO = [20, 10, 5]
 
@@ -102,7 +111,15 @@ if __name__ == '__main__':
             model_architecture = args.network
 
         df_list = list()
+        f1_ratio = {}
+        auc_ratio = {}
+
         for ratio in REDUCTION_RATIO:
             print("{0} at {1} reduction ratio".format(dataset_name, ratio))
-            create_report(dataset_name, ratio, model_architecture, LOSS_LIST)
+            f1, _auc = create_report(dataset_name, ratio, model_architecture, LOSS_LIST)
+            f1_ratio[ratio] = f1
+            auc_ratio[ratio] = _auc
+
+        for loss in LOSS_LIST:
+            print('{0} & {1:.2f} & {2:.2f} & {3:.2f} & {4:.2f} & {5:.2f} & {6:.2f}'.format(loss, np.average(f1_ratio[20][loss]) * 100, np.average(f1_ratio[10][loss]) * 100, np.average(f1_ratio[5][loss]) * 100, np.average(auc_ratio[20][loss]) * 100, np.average(auc_ratio[10][loss]) * 100, np.average(auc_ratio[5][loss]) * 100))
 
