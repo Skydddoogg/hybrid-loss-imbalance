d4778f7259fc9f32dc28c0d9021aba9bc605ad23
Kykung <59070071@it.kmitl.ac.th>
2020-06-13 02:05:22 +0700

    Edit viz error tool

--- visualization_tools/report_error_cifar100.py
+++ visualization_tools/report_error_cifar100.py
@@ -56,9 +56,10 @@ if __name__ == "__main__":
     LOSS_LIST = [
         'MFE',
         'MSFE',
-        'Balanced-FL',
-        'Balanced-Hybrid',
+        'FL',
+        'Hybrid',
         ]
+    N_ROUND = 5
     pos_error_ratio = {}
     neg_error_ratio = {}
     for ratio in [20, 10, 5]:
@@ -68,16 +69,26 @@ if __name__ == "__main__":
         for loss in LOSS_LIST:
             print(loss, '----------------------------------------------------')
             classification_algorithm = 'dl-' + loss
-            y_true = np.load(os.path.join(result_path, dataset_name, 'groundtruth', network + '_' + 'round_' + str(1) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(ratio) + ".npy"))
-            y_pred_prob = np.load(os.path.join(result_path, dataset_name, 'probability', network + '_' + 'round_' + str(1) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(ratio) + ".npy"))
-            y_pred = np.load(os.path.join(result_path, dataset_name, 'prediction', network + '_' + 'round_' + str(1) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(ratio) + ".npy"))
-            _pos, _neg = compute_error(y_true, y_pred, y_pred_prob)
+            pos_error_list = []
+            neg_error_list = []
+            for _round in range(1, N_ROUND + 1):
+                y_true = np.load(os.path.join(result_path, dataset_name, 'groundtruth', network + '_' + 'round_' + str(1) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(ratio) + ".npy"))
+                y_pred_prob = np.load(os.path.join(result_path, dataset_name, 'probability', network + '_' + 'round_' + str(1) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(ratio) + ".npy"))
+                y_pred = np.load(os.path.join(result_path, dataset_name, 'prediction', network + '_' + 'round_' + str(1) + '_' + classification_algorithm + '_' + dataset_name + '_' + str(ratio) + ".npy"))
+                _pos, _neg = compute_error(y_true, y_pred, y_pred_prob)
 
-            pos_error_loss[loss] = _pos
-            neg_error_loss[loss] = _neg
+                pos_error_list.append(_pos)
+                neg_error_list.append(_neg)
+
+            pos_error_loss[loss] = np.array(pos_error_list)
+            neg_error_loss[loss] = np.array(neg_error_list)
 
         pos_error_ratio[ratio] = pos_error_loss
         neg_error_ratio[ratio] = neg_error_loss
 
     for loss in LOSS_LIST:
-        print('{0} & {1:.2f} & {2:.2f} & {3:.2f} & {4:.2f} & {5:.2f} & {6:.2f} & {7:.2f} & {8:.2f} & {9:.2f}'.format(loss, pos_error_ratio[20][loss], pos_error_ratio[10][loss], pos_error_ratio[5][loss], neg_error_ratio[20][loss], neg_error_ratio[10][loss], neg_error_ratio[5][loss], (neg_error_ratio[20][loss] + pos_error_ratio[20][loss]) / 2, (neg_error_ratio[10][loss] + pos_error_ratio[10][loss]) / 2, (neg_error_ratio[5][loss] + pos_error_ratio[5][loss]) / 2))
+        mean_error_20 = np.array([neg_error_ratio[20][loss], pos_error_ratio[20][loss]])
+        mean_error_10 = np.array([neg_error_ratio[10][loss], pos_error_ratio[10][loss]])
+        mean_error_5 = np.array([neg_error_ratio[5][loss], pos_error_ratio[5][loss]])
+
+        print('{0} & {1:.2f}\pm{2:.2f} & {3:.2f}\pm{4:.2f} & {5:.2f}\pm{6:.2f} & {7:.2f}\pm{8:.2f} & {9:.2f}\pm{10:.2f} & {11:.2f}\pm{12:.2f} & {13:.2f}\pm{14:.2f} & {15:.2f}\pm{16:.2f} & {17:.2f}\pm{18:.2f}'.format(loss, np.average(pos_error_ratio[20][loss]), np.std(pos_error_ratio[20][loss]), np.average(pos_error_ratio[10][loss]), np.std(pos_error_ratio[10][loss]), np.average(pos_error_ratio[5][loss]), np.std(pos_error_ratio[5][loss]), np.average(neg_error_ratio[20][loss]), np.std(neg_error_ratio[20][loss]), np.average(neg_error_ratio[10][loss]), np.std(neg_error_ratio[10][loss]), np.average(neg_error_ratio[5][loss]), np.std(neg_error_ratio[5][loss]), np.average(mean_error_20), np.std(mean_error_20), np.average(mean_error_10), np.std(mean_error_10), np.average(mean_error_5), np.std(mean_error_5)))
